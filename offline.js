48386242f14f29efef4c373f066563f88a670d2d
Vahan Asatryan <40138995+umood@users.noreply.github.com>
2018-06-11 22:05:55 +0400

    version 3
    pawn eligibility chack

--- offline.js
+++ offline.js
@@ -1,1 +1,5 @@
+<<<<<<< HEAD
+{"version":1528739945760,"fileList":["channel.html","c2runtime.js","data.js","start.js","style.css","offlineClient.js","images/shared-0-sheet0.png","images/shared-0-sheet1.png","images/tiledbackground-sheet0.png","register-sw.js"]}
+=======
 {"version":1528692590763,"fileList":["channel.html","c2runtime.js","data.js","start.js","style.css","offlineClient.js","images/tiledbackground-sheet0.png","images/shared-0-sheet0.png","images/shared-0-sheet1.png","images/shared-0-sheet3.png","images/shared-0-sheet2.png","register-sw.js"]}
+>>>>>>> 09f99ba64f3df6fb310a1abd1f554402ee234655
